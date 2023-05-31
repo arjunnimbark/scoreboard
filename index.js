bf30144bf04e8a4bf7dbce754b6cf4ae4f7461e6
arjunnimbark <116814201+arjunnimbark@users.noreply.github.com>
2023-05-31 09:47:19 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,41 @@
+let homePts=document.getElementById('home-pts')
+let homeCount=0
+let guestPts=document.getElementById('guest-pts')
+let guestCount=0
+const submit=document.getElementById("reset") 
+
+function homeAddPoint() {
+    homeCount=homeCount+1
+    homePts.innerText=homeCount    
+}
+
+function homeAddTwoPoints() {
+    homeCount=homeCount+2
+    homePts.innerText=homeCount
+}
+
+function homeAddThreePoints() {
+    homeCount=homeCount+3
+    homePts.innerText=homeCount
+}
+
+
+function guestAddPoint() {
+    guestCount=guestCount+1
+    guestPts.innerText=guestCount    
+}
+
+function guestAddTwoPoints() {
+    guestCount=guestCount+2
+    guestPts.innerText=guestCount
+}
+
+function guestAddThreePoints() {
+    guestCount=guestCount+3
+    guestPts.innerText=guestCount
+}
+submit.addEventListener('click',()=>{
+    homeCount=guestCount=0
+    guestPts.innerText=0
+    homePts.innerText=0
+})
